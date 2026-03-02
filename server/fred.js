3687e5aec1b45d237b3cae9b854fff085cf4d24f
ishtiaq <ishtiaqa@buffalo.edu>
2026-03-02 17:12:19 -0500

    Hardcoded vercel link

--- server/fred.js
+++ server/fred.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: 'https://freddashboard.vercel.app',
   methods: ['GET', 'POST'],
   credentials: true
 }));
